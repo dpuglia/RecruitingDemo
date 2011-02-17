dae2a5ab80359209f72f5e92e4643352b84388aa
dpuglia <dpuglia@timbasoftware.co>
2011-02-17 14:45:32 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecruitingDemo::Application.config.secret_token = 'ba017ede7d32dc097119bfdf84b5d23816ba1a8ed71cf174ff34219973d461e1207ee59362ad255d907a1f32b37807ae7980faea1579814a666bc1e903ff313b'
